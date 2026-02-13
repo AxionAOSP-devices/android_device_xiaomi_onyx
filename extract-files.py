9b95be9bccf11eba3099adda7222d8a815c598a1
unmoved21 <unmoved2121@gmail.com>
2026-02-13 14:41:43 +0000

    onyx: Update WFD system blobs from dada OS3.0.6.0.WOCMIXM
    Change-Id: I669970b5faa7c94c48aa56defbd9988969c59f79

--- extract-files.py
+++ extract-files.py
@@ -61,15 +61,6 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/vintf/manifest/vendor.qti.qesdsys.service.xml': blob_fixup()
         .regex_replace(r'(?s)^.*?(?=<manifest)', ''),
-
-    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
-        .add_needed('libgui_shim.so'),
-
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libbinder_shim.so')
-        .add_needed('libinput_shim.so')
-        .remove_needed('android.hidl.base@1.0.so'),
-
     'vendor/etc/sensors/hals.conf': blob_fixup()
         .regex_replace('.*vl53l8.*\n?', ''),
     'vendor/lib64/libaudioserviceexampleimpl.so': blob_fixup()
