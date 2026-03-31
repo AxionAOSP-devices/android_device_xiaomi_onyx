3324bdf40f0fff816bf0c8b11b72f4df5c06c9c7
Zarathos30 <vale.rigni@gmail.com>
2026-03-31 18:23:50 +0200

    Revert "onyx: vendorsetup: Clone MiuiCamera trees"
    This reverts commit 8b2e07150f2a8219d0a1041981e9ca74e8ca4883.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,13 +10,4 @@ echo "Cloning kernel source tree..."
 rm -rf device/xiaomi/onyx-kernel
 git clone -b lineage-23.1 https://github.com/AxionAOSP-devices/android_kernel_xiaomi_onyx.git device/xiaomi/onyx-kernel
 
-# MiuiCamera (fresh clone)
-echo "Cloning MiuiCamera device tree..."
-rm -rf device/xiaomi/onyx-miuicamera
-git clone -b sixteen git@github.com:Zarathos30/device_xiaomi_peridot-miuicamera.git device/xiaomi/onyx-miuicamera
-
-echo "Cloning MiuiCamera vendor tree..."
-rm -rf vendor/xiaomi/onyx-miuicamera
-git clone -b sixteen git@github.com:Zarathos30/vendor_xiaomi_peridot-miuicamera.git vendor/xiaomi/onyx-miuicamera
-
 echo "vendorsetup.sh execution complete."
