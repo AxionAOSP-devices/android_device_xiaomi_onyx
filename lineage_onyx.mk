fa7247392721a3052ae9dab5b7702f4728758903
Zarathos30 <vale.rigni@gmail.com>
2026-08-16 11:38:22 +0200

    onyx: Remove old Axion flags
    Signed-off-by: Zarathos30 <vale.rigni@gmail.com>

--- lineage_onyx.mk
+++ lineage_onyx.mk
@@ -39,7 +39,6 @@ AXION_CAMERA_REAR_INFO := 50,8
 AXION_CAMERA_FRONT_INFO := 20
 
 # Graphics & Display
-TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
 HBM_SUPPORTED := true
 HBM_NODE := /data/vendor/display/hbm_mode
@@ -48,5 +47,3 @@ HBM_NODE := /data/vendor/display/hbm_mode
 BYPASS_CHARGE_SUPPORTED := true
 BYPASS_CHARGE_TOGGLE_PATH ?= /sys/class/xm_power/charger/smart_charge/smart_night
 TARGET_INCLUDE_AXFX := true
-PERF_GOV_SUPPORTED := true
-PERF_DEFAULT_GOV := walt
